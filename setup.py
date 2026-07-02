da117600090ffc051f2e7e08f9b7a478817ef93c
Anurag Patel <anuragpatel4555@gmail.com>
2026-07-02 12:01:07 +0530

    Created setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages,setup
+
+setup(
+    name="Medical-Chatbot",
+    version="0.0.1",
+    author="Anurag Patel",
+    author_email="anuragpatel4555@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+    )
+
